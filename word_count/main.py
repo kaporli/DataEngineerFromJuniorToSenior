ee0ba0ba4290ec1b578238b3172f809ebac4f1bd
Kap <csoreghelias@gmail.com>
2022-10-12 19:29:01 +0200

    Commit

---
+++ word_count/main.py
@@ -0,0 +1,2 @@
+s = "The English Wikipedia is, along with the Simple English Wikipedia, one of two English-language editions of Wikipedia, an online encyclopedia. It was founded on 15 January 2001 as Wikipedia's first edition and, as of 12 October 2022, has the most articles of any edition, at 6,561,058.[1] As of October 2022, 11% of articles in all Wikipedias belong to the English-language edition; this share was more than 50% in 2003.[2][3] The edition's one-billionth edit was made on 13 January 2021.[4]"
+print(len(s.split()));
